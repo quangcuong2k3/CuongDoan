964b2590100c8b367d6920a30ef5671e9d4d8dad
Cuong <cuongquangle539@gmail.com>
2024-11-21 21:26:23 +0700

    video 96

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -5,6 +5,9 @@ import { BsFillPlusCircleFill } from "react-icons/bs";
 import { BsPatchMinusFill } from "react-icons/bs";
 import { AiOutlineMinusCircle } from "react-icons/ai";
 import { AiTwotonePlusSquare } from "react-icons/ai";
+import { RiImageAddFill } from 'react-icons/ri'
+import { v4 as uuidv4 } from 'uuid';
+import _ from 'lodash';
 const Questions = (props) => {
     const options = [
         { value: 'chocolate', label: 'Chocolate' },
@@ -12,6 +15,125 @@ const Questions = (props) => {
         { value: 'vanilla', label: 'Vanilla' },
     ];
     const [selectedQuiz, setSelectedQuiz] = useState([]);
+
+    const [questions, setQuestions] = useState(
+        [
+            {
+                id: uuidv4(),
+                description: ' ',
+                imageFile: '',
+                imageName: '',
+                answers: [
+                    {
+                        id: uuidv4(),
+                        description: ' ',
+                        isCorrect: false
+                    }
+                ]
+
+            },
+
+        ]
+    )
+
+    console.log("check questions", questions)
+
+    const handleAddRemoveQuestion = (type, id) => {
+        if (type === 'ADD') {
+            const newQuestion =
+            {
+                id: uuidv4(),
+                description: '',
+                imageFile: '',
+                imageName: '',
+                answers: [
+                    {
+                        id: uuidv4(),
+                        description: '',
+                        isCorrect: false
+                    },
+
+                ]
+
+            };
+            setQuestions([...questions, newQuestion]);
+        }
+        if (type === 'REMOVE') {
+            let questionsClone = _.cloneDeep(questions);
+            questionsClone = questionsClone.filter(item => item.id !== id);
+            setQuestions(questionsClone);
+        }
+
+    }
+
+
+    const handleAddRemoveAnswer = (type, questionId, answerId) => {
+        let questionsClone = _.cloneDeep(questions);
+        if (type === 'ADD') {
+            const newAnswer =
+            {
+                id: uuidv4(),
+                description: '',
+                isCorrect: false,
+
+            };
+            let index = questionsClone.findIndex(item => item.id === questionId);
+            questionsClone[index].answers.push(newAnswer)
+            setQuestions(questionsClone);
+        }
+        if (type === 'REMOVE') {
+            let index = questionsClone.findIndex(item => item.id === questionId);
+            questionsClone[index].answers =
+                questionsClone[index].answers.filter(item => item.id !== answerId)
+            setQuestions(questionsClone);
+        }
+    }
+    const handleOnChange = (type, questionId, value) => {
+        if (type === "QUESTION") {
+            let questionsClone = _.cloneDeep(questions);
+            let index = questionsClone.findIndex(item => item.id === questionId);
+            if (index > -1) {
+                questionsClone[index].description = value;
+                setQuestions(questionsClone);
+            }
+        }
+    }
+    const handleOnChangeFileQuestion = (questionId, event) => {
+        let questionsClone = _.cloneDeep(questions);
+        let index = questionsClone.findIndex(item => item.id === questionId);
+        if (index > -1 && event.target && event.target.files && event.target.files[0]) {
+            questionsClone[index].imageFile = event.target.files[0];
+            questionsClone[index].imageName = event.target.files[0].name;
+            setQuestions(questionsClone);
+        }
+    }
+
+    const handleAnswerQuestion = (type, answerId, questionId, value) => {
+        let questionsClone = _.cloneDeep(questions);
+        let index = questionsClone.findIndex(item => item.id === questionId);
+
+        if (index > -1) {
+            questionsClone[index].answers =
+                questionsClone[index].answers.map(answer => {
+                    if (answer.id === answerId) {
+                        if (type === 'CHECKBOX') {
+                            answer.isCorrect = value;
+                        }
+                        if (type === 'INPUT') {
+                            answer.description = value;
+                        }
+                    }
+                    return answer;
+                })
+
+            setQuestions(questionsClone);
+        }
+    }
+
+    const handleSubmitQuestionForQuiz = () => {
+        console.log("question", questions)
+    }
+
     return (
         <div className="questions-container">
             <div className="title">
@@ -19,7 +141,7 @@ const Questions = (props) => {
             </div>
             <div className="add-new-questions">
                 <div className='col-6 from-group'>
-                    <label>Select Quiz</label>
+                    <label className='mb-2'>Select Quiz</label>
                 </div>
                 <Select
                     value={selectedQuiz}
@@ -28,51 +150,102 @@ const Questions = (props) => {
                 />
             </div>
 
-            <div className='mt-3 '>
+            <div className='mt-3 mb-2'>
                 Add Question:
             </div>
-            <div>
-                <div className='questions-content'>
 
-                    <div className="form-floating description">
-                        <input type="type" className="form-control" placeholder="name@example.com" />
-                        <label >description</label>
-                    </div>
-                    <div className='group-upload'>
-                        <label className='label-up'>
-                            Upload Image
-                        </label>
-                        <input type={'file'} hidden />
-                        <span>0 file is uploaded</span>
-                    </div>
-                    <div className='btn-add'>
-                        <span>
-                            <BsFillPlusCircleFill className='icon-add' />
-                        </span>
-                        <span>
-                            <BsPatchMinusFill className='icon-remove' />
-                        </span>
-                    </div>
+            {
+                questions && questions.length > 0
+                && questions.map((question, index) => {
+                    return (
+                        <div key={question.id} className='q-main mb-4'>
+                            <div className='questions-content'>
+
+                                <div className="form-floating description">
+                                    <input type="type"
+                                        className="form-control"
+                                        placeholder="name@example.com"
+                                        value={question.description}
+                                        onChange={(event) => handleOnChange('QUESTION', question.id, event.target.value)}
+                                    />
+                                    <label >Question {index + 1}'s description</label>
+                                </div>
+                                <div className='group-upload'>
+                                    <label htmlFor={`${question.id}`}>
+                                        <RiImageAddFill className='label-up' />
+
+                                    </label>
+                                    <input
+                                        id={`${question.id}`}
+                                        onChange={(event) => handleOnChangeFileQuestion(question.id, event)}
+                                        type={'file'}
+                                        hidden
+
+                                    />
+                                    <span>{question.imageName ? question.imageName : "0 file is uploaded"}</span>
+                                </div>
+                                <div className='btn-add'>
+                                    <span onClick={() => handleAddRemoveQuestion('ADD', '')}>
+                                        <BsFillPlusCircleFill className='icon-add' />
+                                    </span>
+                                    {questions.length > 1 &&
+                                        <span onClick={() => handleAddRemoveQuestion('REMOVE', question.id)}>
+                                            <BsPatchMinusFill className='icon-remove' />
+                                        </span>
+                                    }
+                                </div>
+                            </div>
+
+                            {question.answers && question.answers.length > 0
+                                && question.answers.map((answer, index) => {
+                                    return (
+                                        <div key={answer.id} className='answers-content'>
+                                            <input
+                                                className="form-check-input iscorrect"
+                                                type="checkbox"
+                                                checked={answer.isCorrect}
+                                                onChange={(event) => handleAnswerQuestion('CHECKBOX', answer.id, question.id, event.target.checked)}
+                                            />
+                                            <div className="form-floating answer-name">
+                                                <input
+                                                    value={answer.description}
+                                                    type="type"
+                                                    className="form-control"
+                                                    placeholder="name@example.com"
+                                                    checked={answer.isCorrect}
+                                                    onChange={(event) => handleAnswerQuestion('INPUT', answer.id, question.id, event.target.value)}
+                                                />
+                                                <label >Answer {index + 1}</label>
+                                            </div>
+                                            <div className='btn-group'>
+                                                <span onClick={() => handleAddRemoveAnswer('ADD', question.id)}>
+                                                    <AiTwotonePlusSquare className='icon-add' />
+                                                </span>
+
+                                                {question.answers.length > 1 &&
+                                                    <span onClick={() => handleAddRemoveAnswer('REMOVE', question.id, answer.id)}>
+                                                        <AiOutlineMinusCircle className='icon-remove' />
+                                                    </span>
+                                                }
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    )
+                })
+            }
+            {
+                questions && questions.length > 0 &&
+                <div>
+                    <button
+                        onClick={() => handleSubmitQuestionForQuiz()}
+                        className='btn btn-warning'>
+                        Save Question
+                    </button>
                 </div>
-                <div className='answers-content'>
-                    <input
-                        className="form-check-input iscorrect"
-                        type="checkbox"
-                    />
-                    <div className="form-floating answer-name">
-                        <input type="type" className="form-control" placeholder="name@example.com" />
-                        <label >Answer 1</label>
-                    </div>
-                    <div className='btn-group'>
-                        <span>
-                            <AiTwotonePlusSquare className='icon-add' />
-                        </span>
-                        <span>
-                            <AiOutlineMinusCircle className='icon-remove' />
-                        </span>
-                    </div>
-                </div>
-            </div>
+            }
         </div>
 
     )
